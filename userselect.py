ec1735975b66f45d9e19a25ef63844a30600574a
Christian D Tuen <christiandt@gmail.com>
2014-02-16 03:12:47 -0800

    Cleaned up, added refresh button+function

--- userselect.py
+++ userselect.py
@@ -7,14 +7,13 @@ from chat import Chat
 
 class userSelect(QtGui.QDialog):
 
-    def userChanged(self, item):
-        self.user = item
-
     def chatStarted(self):
-        user = self.list.currentItem().text()
-        ip = self.userList[str(user)]
-        if ip != None:
-            self.chat = Chat(user, ip)
+        clickedItem = self.list.currentItem()
+        if clickedItem != None:
+            user = self.list.currentItem().text()
+            ip = self.userList[str(user)]
+            if ip != None:
+                self.chats.append(Chat(user, ip))
         #self.accept()
 
     def getUsers(self):
@@ -25,12 +24,19 @@ class userSelect(QtGui.QDialog):
         else:
             return {'Server error':None}
 
+    def refreshUsers(self):
+        self.userList = self.getUsers()
+        users = list(self.userList)
+        self.list.clear()
+        self.list.addItems(users)
+
     def quitProgram(self):
         sys.exit(0)
 
     def __init__(self, username):
         super(userSelect, self).__init__()
         self.username = username
+        self.chats = []
         title = "Users"
         self.setWindowTitle(str(title))
         self.resize(300,400)
@@ -42,7 +48,7 @@ class userSelect(QtGui.QDialog):
         self.userList = self.getUsers()
         users = list(self.userList)
         self.list.addItems(users)
-        self.layout.addWidget(self.list, 0, 0, 1, 2)
+        self.layout.addWidget(self.list, 0, 0, 1, 3)
 
         #selectbutton
         self.selectbutton = QtGui.QPushButton(self)
@@ -52,13 +58,20 @@ class userSelect(QtGui.QDialog):
         self.layout.addWidget(self.selectbutton, 1, 0)
         QtCore.QObject.connect(self.selectbutton, QtCore.SIGNAL("clicked()"), self.chatStarted)
 
+        #refreshbutton
+        self.refreshbutton = QtGui.QPushButton(self)
+        self.refreshbutton.setText("Refresh")
+        self.refreshbutton.setMinimumWidth(20)
+        self.refreshbutton.setMinimumHeight(50)
+        self.layout.addWidget(self.refreshbutton, 1, 1)
+        QtCore.QObject.connect(self.refreshbutton, QtCore.SIGNAL("clicked()"), self.refreshUsers)
 
         #cancelbutton
         self.cancelbutton = QtGui.QPushButton(self)
         self.cancelbutton.setText("Cancel")
         self.cancelbutton.setMinimumWidth(5)
         self.cancelbutton.setMinimumHeight(50)
-        self.layout.addWidget(self.cancelbutton, 1, 1)
+        self.layout.addWidget(self.cancelbutton, 1, 2)
         QtCore.QObject.connect(self.cancelbutton, QtCore.SIGNAL("clicked()"), self.quitProgram)
 
         self.setLayout(self.layout)
